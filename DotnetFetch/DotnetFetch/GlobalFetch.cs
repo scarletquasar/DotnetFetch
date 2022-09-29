22b53136a8d3e3f508dd79b7ebe5078dd7b99365
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-09-29 12:40:49 -0300

    feat(wip): deconstructing options object to get fetch options

--- DotnetFetch/DotnetFetch/GlobalFetch.cs
+++ DotnetFetch/DotnetFetch/GlobalFetch.cs
@@ -7,12 +7,11 @@ namespace DotnetFetch
     {
         public static async Task<Response> Fetch(string resource, JsonObject options)
         {
-            var headers = 
-                options.TryGetPropertyValue("headers", out var _headers) ? _headers : new JsonObject();
-
-            var body =
-                options.TryGetPropertyValue("body", out var _body) ? _body : new JsonObject();
-
+            var headers = options["headers"] ?? new JsonObject();
+            var body = options["body"] ?? new JsonObject();
+            var method = options["method"]?.ToString() ?? "get"; //get | post | delete | put | patch | head |options
+            var mode = options["mode"]?.ToString() ?? "no-cors"; //cors | no-cors | same-origin
+            var credentials = options["credentials"]?.ToString() ?? "same-orgin"; //omit | same-origin | include
 
 
             return new();
