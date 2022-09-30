91d04cdea1a7c5301a9cad16bfe793c1eb4588b6
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-09-29 22:52:49 -0300

    feat: finished dotnet fetch basic implementation with tests

---
+++ DotnetFetch.Tests/FetchTests.cs
@@ -0,0 +1,20 @@
+using DotnetFetch;
+using System.Text.Json;
+
+namespace DotnetFetch.Tests
+{
+    public class FetchTests
+    {
+        [Fact]
+        public async void FetchBasicFunctionShouldWorkProperly()
+        {
+            var result = await GlobalFetch.Fetch("https://jsonplaceholder.typicode.com/todos/1");
+            var model = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(result.Text())!;
+
+            Assert.Equal(1, model["userId"].GetInt32());
+            Assert.Equal(1, model["id"].GetInt32());
+            Assert.Equal("delectus aut autem", model["title"].GetString());
+            Assert.False(model["completed"].GetBoolean());
+        }
+    }
+}
