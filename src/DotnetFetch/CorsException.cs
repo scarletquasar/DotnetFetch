e0f7ec5382ac6ee59fe6523c0d48dc3d2af7ac1a
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2023-12-31 19:02:21 -0600

    Derived `CorsException` from `FetchCorsException`.

--- src/DotnetFetch/CorsException.cs
+++ src/DotnetFetch/CorsException.cs
@@ -1,17 +1,31 @@
+using DotnetFetch.Models;
+
 namespace DotnetFetch;
 
 /// <summary>
 /// Represents an exception thrown when a CORS error is detected.
 /// </summary>
+/// <remarks>
+/// This class currently derives from <see cref="FetchCorsException"/> and
+/// subsequently <see cref="ArgumentException"/>. In the future, this derivation will be removed
+/// in favor of a direct derivation from <see cref="Exception"/>.
+/// </remarks>
 public class CorsException
-    : Exception
+
+#pragma warning disable CS0618 // Type or member is obsolete
+
+    // Justification: This derivation is in place to preserve backwards compatibility with the FetchCorsException class.
+    : FetchCorsException
+
+#pragma warning restore CS0618 // Type or member is obsolete
+
 {
     /// <summary>
     /// Creates a new <see cref="CorsException"/> from the specified URL with a default message.
     /// </summary>
     /// <param name="url">The URL that caused the exception.</param>
     public CorsException(string url)
-        : base($"A CORS exception has occurred, check your request options.") =>
+        : base("A CORS exception has occurred, check your request options.") =>
         Url = url;
     
     /// <summary>
